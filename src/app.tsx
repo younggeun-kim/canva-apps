b36ff7ef817918b71abe26208ccb616dd87d2917
kim-young-geun <younggeun@mindslab.ai>
2023-08-19 12:05:24 +0900

    .

--- src/app.tsx
+++ src/app.tsx
@@ -1,28 +1,93 @@
-import { Button, Rows, Text } from "@canva/app-ui-kit";
+import { Button, Rows, Text, MultilineInput } from "@canva/app-ui-kit";
 import { addNativeElement } from "@canva/design";
-import * as React from "react";
+import { auth } from "@canva/user";
+import React, { useState } from "react";
 import styles from "styles/components.css";
 
+type State = "idle" | "loading" | "success" | "error";
+//runpod APIs
+const endpoint_id="jgaeb04ckzrfbv";
+const run_endpoint=`https://api.runpod.ai/v2/${endpoint_id}/run`;
+const health_endpoint=`https://api.runpod.ai/v2/${endpoint_id}/health`;
+
+//runpod header
+const api_key="SKDBTSH6C180KLURALZJG5O6U0BXIOIAWPW941HK";
+const runpod_header={
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${api_key}`
+};
+
+
 export const App = () => {
+  const [inputValue, setInputValue] = React.useState("");
+  const [state, setState] = useState<State>("idle");
+  const [responseBody, setResponseBody] = useState<unknown | undefined>(
+    undefined
+  );
+  const sendGetRequest = async () => {
+    try {
+      setState("loading");
+      const payload={
+        'input': {
+            'text_list': Array.from({ length: 5 }).map(() => inputValue),
+            'duration': 5,
+            'document_id': "",
+        }
+    };
+      const token = await auth.getCanvaUserToken();
+      const res = await fetch(
+        run_endpoint,
+        {
+            method: "POST",
+            headers: runpod_header,
+            body: JSON.stringify(payload),
+        }
+    );
+
+      const body = await res.json();
+      console.log(body);
+      setResponseBody(body);
+      setState("success");
+    } catch (error) {
+      setState("error");
+      console.error(error);
+    }
+  };
+
+
   const onClick = () => {
     addNativeElement({
       type: "TEXT",
-      children: ["Hello world!"],
+      children: [inputValue],
     });
   };
+
+  function handleChange(value: string, event: React.ChangeEvent<HTMLTextAreaElement>) {
+    setInputValue(value);
+    console.log(value);
+}
+
 
   return (
     <div className={styles.scrollContainer}>
       <Rows spacing="2u">
         <Text>
-          To make changes to this app, edit the{" "}
-          <code className={styles.code}>src/app.tsx</code> file, then close and
-          reopen the app in the editor to preview the changes.
+          What sound would you create?
         </Text>
-        <Button variant="primary" onClick={onClick} stretch>
-          Do something codddsf
+          
+        <MultilineInput
+    placeholder="bubble popping" 
+    value={inputValue} 
+    onChange={handleChange}
+/>
+
+        <Button variant="primary" onClick={sendGetRequest} stretch>
+          Generate Sound Effects
+        </Button>
+        <Button variant="secondary" onClick={onClick} stretch>
+          Sign up or Sign in
         </Button>
       </Rows>
-    </div>
+</div>
   );
 };
